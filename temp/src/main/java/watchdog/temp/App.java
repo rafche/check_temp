510e1e7893c62f3850fe83b3f99dc56698143039
rafche <rafche@gmail.com>
2019-06-30 21:59:34 +0200

    using static methods
    create temp_check class
    ...
    
    ...

--- temp/src/main/java/watchdog/temp/App.java
+++ temp/src/main/java/watchdog/temp/App.java
@@ -1,32 +1,18 @@
 package watchdog.temp;
-import java.io.*;
-import java.util.*;
-
+import watchdog.temp.temp_check;
 
 /**
- * checking the temprature of SOC 
+ * checking the temperature of SOC 
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+
+    public static void main(String[] args )
     {
-    	ProcessBuilder processBuilder = new ProcessBuilder();
-    	InputStream stdout = null;
-    	String[] cmd = {"/opt/vc/bin/vcgencmd", "measure_temp"};
-    	String[] ret;
-    	String s = null;
-    	
-    	try {
-			processBuilder.command(cmd);	
-			Process process = processBuilder.start();
-			stdout = process.getInputStream ();
-			BufferedReader out = new BufferedReader (new InputStreamReader (stdout));  
-			while ((s = out.readLine()) != null) { 
-				System.out.println(s);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+        int soc_temp = temp_check.temp_check();
+
+
+        System.out.println(soc_temp);
     }
 }
---
+++ temp/src/main/java/watchdog/temp/temp_check.java
@@ -0,0 +1,38 @@
+package watchdog.temp;
+
+import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class temp_check {
+
+	public static int temp_check() {
+		int temprature = -10;
+		ProcessBuilder processBuilder = new ProcessBuilder();
+    	InputStream stdout = null;
+    	String[] cmd = {"/opt/vc/bin/vcgencmd", "measure_temp"};
+    	String[] ret;
+    	String s = null;
+    	
+    	try {
+			processBuilder.command(cmd);	
+			Process process = processBuilder.start();
+			stdout = process.getInputStream ();
+			BufferedReader out = new BufferedReader (new InputStreamReader (stdout));  
+			s = out.readLine();
+			Pattern p = Pattern.compile("\\d+");
+			Matcher m = p.matcher(s);
+				while (m.find()){
+					temprature = Integer.parseInt(m.group(0));
+					System.out.println(temprature);
+
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return temprature;
+		
+		}
+
+}
